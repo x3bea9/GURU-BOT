afb8d98189962074f4a25ba04a7d6fe6562460ba
x3bea9 <83083327+x3bea9@users.noreply.github.com>
2023-05-24 13:55:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  [966565652079', 'GURU', true],
+  [966565652079', 'Ash', true], 
   [''] 
 ] //Number of owners
 
@@ -47,15 +47,15 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'DUCK MD 🪿'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
+global.packname = '🪿 𝒅𝒖𝒄𝒌 𝒍𝒐𝒗𝒆𝒔 𝒚𝒐𝒖 🩵༅' 
+global.author = '🌧️ ̶͟͞=̶͟͞𝑸𝒖𝒂𝒄𝒌=̶͟͞ Ⴇ┃ᴮᴼᵀ' 
+global.igfg = '▢ 🪿' 
+global.dygp = 'https://chat.whatsapp.com/CG8axZ7iqxE4EOGCuxwksV'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.fgyt = 'null'
+global.fgpyp = 'null'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 global.dbase = 'mongodb+srv://guru:guru@cluster0.qpggl6x.mongodb.net/?retryWrites=true&w=majority' //ADD YOUR MONGODB BY CHANGIMG IT
 
